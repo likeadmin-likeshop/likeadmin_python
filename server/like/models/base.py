75865adf8f373361eb13085c5833affa52642a29
morpheus <625215067@qq.com>
2022-10-29 00:21:29 +0800

    models 修改： 1. 时间相关字段合并TimestampMixin 2. 增加岗位和部门表

--- server/like/models/base.py
+++ server/like/models/base.py
@@ -1,6 +1,14 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, text
+from sqlalchemy.dialects.mysql import INTEGER
 
-__all__ = ['Base']
+__all__ = ['Base', 'TimestampMixin']
 
 # 数据库模型基类
 Base = declarative_base()
+
+
+class TimestampMixin:
+    create_time = Column(INTEGER(10, unsigned=True), nullable=False, server_default=text('0'), comment='创建时间')
+    update_time = Column(INTEGER(10, unsigned=True), nullable=False, server_default=text('0'), comment='更新时间')
+    delete_time = Column(INTEGER(10, unsigned=True), nullable=False, server_default=text('0'), comment='删除时间')
--- server/like/models/system.py
+++ server/like/models/system.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column, String, text
 from sqlalchemy.dialects import mysql
 
-from .base import Base
+from .base import Base, TimestampMixin
 
 __all__ = [
-    'SystemAuthAdmin', 'SystemAuthMenu', 'SystemAuthPerm', 'SystemAuthRole', 'SystemLogLogin',
-    'system_auth_admin', 'system_auth_menu', 'system_auth_perm', 'system_auth_role', 'system_log_login',
+    'SystemAuthAdmin', 'SystemAuthMenu', 'SystemAuthPerm', 'SystemAuthRole', 'SystemLogLogin', 'SystemAuthDept',
+    'SystemAuthPost', 'system_auth_admin', 'system_auth_menu', 'system_auth_perm', 'system_auth_role',
+    'system_log_login',
+    'system_auth_post', 'system_auth_dept'
 ]
 
 
@@ -140,8 +142,53 @@ class SystemLogLogin(Base):
     create_time = Column(mysql.INTEGER(10, unsigned=True), nullable=False, server_default=text('0'), comment='创建时间')
 
 
+class SystemAuthDept(Base, TimestampMixin):
+    __tablename__ = 'la_system_auth_dept'
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8mb4',
+        'mysql_collate': 'utf8mb4_general_ci',
+        'mysql_row_format': 'Dynamic',
+        'mysql_auto_increment': '1',
+        'comment': '系统部门管理表',
+    }
+    id = Column(mysql.INTEGER(10, unsigned=True), primary_key=True, comment='主键')
+    pid = Column(mysql.INTEGER(10, unsigned=True), nullable=False, server_default=text('0'), comment='上级主键')
+    name = Column(String(100), nullable=False, server_default='', comment='部门名称')
+    duty = Column(String(30), nullable=False, server_default='', comment='负责人名')
+    mobile = Column(String(30), nullable=False, server_default='', comment='联系电话')
+    sort = Column(mysql.SMALLINT(5), nullable=False, server_default=text('0'), comment='排序编号')
+    is_stop = Column(mysql.TINYINT(1, unsigned=True), nullable=False, server_default=text('0'),
+                     comment='是否停用: 0=否, 1=是')
+    is_delete = Column(mysql.TINYINT(1, unsigned=True), nullable=False, server_default=text('0'),
+                       comment='是否删除: [0=否, 1=是]')
+
+
+class SystemAuthPost(Base, TimestampMixin):
+    __tablename__ = 'la_system_auth_post'
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8mb4',
+        'mysql_collate': 'utf8mb4_general_ci',
+        'mysql_row_format': 'Dynamic',
+        'mysql_auto_increment': '1',
+        'comment': '系统岗位管理表',
+    }
+    id = Column(mysql.INTEGER(10, unsigned=True), primary_key=True, comment='主键')
+    code = Column(String(30), nullable=False, server_default=text('0'), comment='岗位编码')
+    name = Column(String(30), nullable=False, server_default='', comment='岗位名称')
+    remarks = Column(String(250), nullable=False, server_default='', comment='岗位备注')
+    sort = Column(mysql.SMALLINT(5), nullable=False, server_default=text('0'), comment='排序编号')
+    is_stop = Column(mysql.TINYINT(1, unsigned=True), nullable=False, server_default=text('0'),
+                     comment='是否停用: 0=否, 1=是')
+    is_delete = Column(mysql.TINYINT(1, unsigned=True), nullable=False, server_default=text('0'),
+                       comment='是否删除: [0=否, 1=是]')
+
+
 system_auth_admin = SystemAuthAdmin.__table__
 system_auth_menu = SystemAuthMenu.__table__
 system_auth_perm = SystemAuthPerm.__table__
 system_auth_role = SystemAuthRole.__table__
 system_log_login = SystemLogLogin.__table__
+system_auth_post = SystemAuthPost.__table__
+system_auth_dept = SystemAuthDept.__table__
