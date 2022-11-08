01a8715ce597a814aeb17ff06511a8619158f8f0
geeknonerd <geek@best2geek.com>
2022-11-09 07:54:27 +0800

    修改"缓存监控"接口info中字典返回格式

--- server/like/admin/routers/monitor.py
+++ server/like/admin/routers/monitor.py
@@ -16,9 +16,18 @@ router = APIRouter(prefix='/monitor')
 async def monitor_cache():
     """缓存监控"""
     info = await RedisUtil.info()
+    res_info = {}
+    for key, value in info.items():
+        if isinstance(value, dict):
+            value = ','.join({f'{k}={v}' for k, v in value.items()})
+        else:
+            value = str(value)
+        res_info[key] = value
+
     db_size = await RedisUtil.dbsize()
+
     command_stats = await RedisUtil.info('commandstats')
     stats_list = []
     for k, v in command_stats.items():
         stats_list.append({'name': k.split('_')[-1], 'value': str(v.get('calls', ''))})
-    return {'info': {k: str(v) for k, v in info.items()}, 'commandStats': stats_list, 'dbSize': db_size}
+    return {'info': res_info, 'commandStats': stats_list, 'dbSize': db_size}
