3635fb6ef8f8eb89bea8222d32e5d60a6dca6b1c
Yuuux <742686855@qq.com>
2022-12-02 19:01:53 +0800

    打包

--- admin/src/config/index.ts
+++ admin/src/config/index.ts
@@ -1,8 +1,17 @@
+const baseUrl=() =>{
+    let url=window.location.href
+    let urlArr=url.split('.com')
+    let urlArrPrev=urlArr[0]
+    let webUrl =urlArrPrev.toString()+'.com/'
+    return webUrl
+}
+
 const config = {
     terminal: 1, //终端
     title: '后台管理系统', //网站默认标题
     version: '1.3.3', //版本号
-    baseUrl: `${import.meta.env.VITE_APP_BASE_URL || ''}/`, //请求接口域名
+    // baseUrl: `${import.meta.env.VITE_APP_BASE_URL || ''}/`, //请求接口域名
+    baseUrl: baseUrl(), //请求接口域名
     urlPrefix: 'api', //请求默认前缀
     timeout: 10 * 1000 //请求超时时长
 }
