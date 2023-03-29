448bf116cce7f299af0bd5fbd97f68aecdb60847
Yuuux <742686855@qq.com>
2023-03-29 11:30:17 +0800

    前台打包

--- public/mobile/assets/shop.5d8d93f6.js
+++
@@ -1,1 +0,0 @@
-import{C as r}from"./index.0362d7aa.js";function t(){return r.get({url:"/index"})}function a(t){return r.get({url:"/decorate",data:t})}function e(){return r.get({url:"/hotSearch"})}function n(t){return r.get({url:"/search",data:t})}export{a,e as b,n as c,t as g};
